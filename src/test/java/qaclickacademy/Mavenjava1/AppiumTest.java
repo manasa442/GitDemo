3533e2e95ec6409757e1d1ceb69bd623cb1aa218
unknown <442manasa@gmail.com>
2020-12-13 20:22:39 +0530

    Commit message

---
+++ src/test/java/qaclickacademy/Mavenjava1/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava1;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+@Test
+public void NativeAPpAndroid()
+{
+	System.out.println("NativeAPpAndroid");	
+}
+@Test
+public void BrowserAutomation()
+{
+	System.out.println("BrowserAutomation");
+}
+
+}
